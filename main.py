8f8ae2b79beaa46d8613e4d23c9a52126c343be4
Adley Wong <adleywong59@gmail.com>
2017-03-14 19:44:34 -0700

    update
    for some reason, code isn't being pushed

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ from setAccess import visionCalls
 g = GripPipeline()
 v = visionCalls()
 calls = ""
+autoMode = True
+vision = "vision"
+stream = "stream"
 
 '''
-x = 0
-ip = "http://127.0.0.1:1180/?action=stream"
-cap = cv.VideoCapture()
-
-p = None
+The main method that will run all the vision based commands and mjpeg streamer
+this function uses networktables in order to recieve data and enable autonomous streamer
+or during teleop vision and mjpeg streamer
 '''
-while(True):
-    if (g.cameraStatus() != -1):
-        if (g.cameraStatus() == 0):
-            calls = "stream"
-        elif (g.cameraStatus() == 1):
-            calls = "vision"
-        methodCall = getattr(v, calls)
+
+while (True):
+    while (g.autoMode() == 1):
+        methodCall = getattr(v, "vision")
         methodCall()
 
-    '''
-        if(g.filter_contours_output is not 0):
-            ret, frame = cap.read()
-            g.cameraOnline()
-            g.process(frame)
-    '''
+    while (g.autoMode() == 0):
+        if (g.cameraStatus() != -1):
+            if (g.cameraStatus() == 0):
+                calls = stream
+            elif (g.cameraStatus() == 1):
+                calls = vision
+            methodCall = getattr(v, calls)
+            methodCall()
+    while(g.autoMode() == -1):
+        continue
--- setAccess.py
+++ setAccess.py
@@ -4,14 +4,25 @@ import cv2 as cv
 import subprocess
 
 class visionCalls:
+
+    '''
+    Initializing variables
+    '''
     def __init__(self):
         self.x = 0
         self.g = GripPipeline()
         self.cap = cv.VideoCapture()
         self.p = None
+        self.once = True
 
+    '''
+    turns on streamer and closes opencv vision
+    '''
     def stream(self):
-        self.cap.release()
+        if(self.once == False):
+            self.cap.release()
+            self.once = True
+
         if (self.x == 0):
             # print("Starting stream")
             self.x = 1
@@ -19,21 +30,29 @@ class visionCalls:
                 [
                     "mjpg_streamer",
                     "-i",
-                    "input_uvc.so -d /dev/video0 -f 15 -r 640x480 -y -n",
+                    "input_uvc.so -d /dev/video0 -f 30 -r 640x480 -y -n",
                     "-o",
                     "output_http.so -w ./www -p 1180",
                     "&"
                 ])
 
+    '''
+    Enables opencv and runs the process
+    '''
     def vision(self):
         if (self.x == 1):
             ##print("Ending process")
             self.p.kill()
-            time.sleep(2)
+            time.sleep(2.5)
             self.x = 0
+            self.once = True
 
-        self.cap.open(0)
+        if(self.once):
+            self.cap.open(0)
+            self.once = False
+
         # print("Using vision")
         ret, frame = self.cap.read()
         #.cameraOnline()
         self.g.process(frame)
+
